e2c7b14655c26dafbfa128dd75dda3650141b52f
DESKTOP-OF4KGST\Юра <khabal270@gmail.com>
2023-04-08 19:22:44 +0300

    First commit

---
+++ MarketWeb/Controllers/CategoryController.cs
@@ -0,0 +1,25 @@
+﻿using MarketWeb.Data;
+using MarketWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MarketWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db) 
+        { 
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            var getObjectCategory = _db.Categories.ToList();
+            return View(getObjectCategory);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
