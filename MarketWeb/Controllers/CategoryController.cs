7573e112d089255365277c38d4e0ce744d5b2d9c
DESKTOP-OF4KGST\Юра <khabal270@gmail.com>
2023-04-15 13:33:00 +0300

    CRUD Operations

--- MarketWeb/Controllers/CategoryController.cs
+++ MarketWeb/Controllers/CategoryController.cs
@@ -7,8 +7,8 @@ namespace MarketWeb.Controllers
     public class CategoryController : Controller
     {
         private readonly ApplicationDbContext _db;
-        public CategoryController(ApplicationDbContext db) 
-        { 
+        public CategoryController(ApplicationDbContext db)
+        {
             _db = db;
         }
         public IActionResult Index()
@@ -24,8 +24,8 @@ namespace MarketWeb.Controllers
 
         [HttpPost]
         public IActionResult Create(Category obj)
-        {   
-            /*if(obj.Name == obj.DisplayOrder.ToString())
+        {
+            /*if (obj.Name == obj.DisplayOrder.ToString())
             {
                 ModelState.AddModelError("name", "The DisplayOrder cannot exactly math the Name.");
             }*/
@@ -34,9 +34,75 @@ namespace MarketWeb.Controllers
             {
                 _db.Categories.Add(obj);
                 _db.SaveChanges();
+                TempData["success"] = "Category created successfully";
                 return RedirectToAction("Index");
             }
             return View();
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            Category? categoryFromDb = _db.Categories.Find(id);
+            /*Category? categoryFromDb1 = _db.Categories.FirstOrDefault(u => u.Id == id);
+            Category? categoryFromDb2 = _db.Categories.Where(u => u.Id == id).FirstOrDefault();*/
+
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+            if(ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                TempData["success"] = "Category updated successfully";
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            Category? categoryFromDb = _db.Categories.Find(id);
+            /*Category? categoryFromDb1 = _db.Categories.FirstOrDefault(u => u.Id == id);
+            Category? categoryFromDb2 = _db.Categories.Where(u => u.Id == id).FirstOrDefault();*/
+
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePOST(int? id)
+        {
+            Category? obj = _db.Categories.Find(id);
+
+            if(obj == null)
+            {
+                return NotFound();
+            }
+            _db.Categories.Remove(obj);
+            _db.SaveChanges();
+            TempData["success"] = "Category deleted successfully";
+            return RedirectToAction("Index");
         }
     }
 }
